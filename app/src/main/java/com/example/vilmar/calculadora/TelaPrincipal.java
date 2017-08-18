34be88adbeda4dc9248bf9ad38596a3bcbb6ba4a
vilmarferreira <ferreiragomes7792@gmail.com>
2017-08-18 08:54:35 -0300

    Modificações com calculos basicos
    atualização com calculos basicos

--- app/src/main/java/com/example/vilmar/calculadora/TelaPrincipal.java
+++ app/src/main/java/com/example/vilmar/calculadora/TelaPrincipal.java
@@ -1,16 +1,20 @@
 package com.example.vilmar.calculadora;
 
+import android.media.MediaCodec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class TelaPrincipal extends AppCompatActivity {
 
     private TextView __textoView;// variavel que será usada para armazenar o texto que tiver no textview da tela
     private String __Texto="";// variavel que será usada para atualizar o texto da tela na textview
-    private  String Operador;
+    private  String Operador="";
 
      @Override
      protected void onCreate(Bundle savedInstanceState)
@@ -48,4 +52,33 @@ public class TelaPrincipal extends AppCompatActivity {
         clear();
         updateTela();
     }
+    public double operacao (String a, String b, String op)
+    {
+        switch (op)
+        {
+            case "+": return Double.valueOf(a) + Double.valueOf(b);
+            case "-": return Double.valueOf(a) - Double.valueOf(b);
+            case "X": return Double.valueOf(a)* Double.valueOf(b);
+            case "/":
+                try {
+                    return Double.valueOf(a) / Double.valueOf(b);
+                }catch (Exception e)
+                {
+                        Log.d("Calc",e.getMessage());
+                }
+            default: return -1;
+        }
+
+    }
+
+    public void onClickEqual (View v)
+    {
+        String[] numeros= __Texto.split(Pattern.quote(Operador));
+        if(numeros.length<2) return ;
+
+        double result=operacao(numeros[0],numeros[1],Operador);
+
+        __textoView.setText(__Texto+"\n"+ String.valueOf(result));
+    }
+
 }
